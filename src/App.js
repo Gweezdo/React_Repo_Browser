67b1c7d47a4ab83a5eb77dacf9bdd3343a1801dc
Gweezdo <z.trivium@gmail.com>
2021-01-18 20:28:59 +1100

    Updated node-sass to version that is suported, imported header into App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import React from 'react';
+
+import Header from './components/header/header.component'
 
 import './App.css';
 
 function App() {
   return (
-    
+    <div>  
+      <Header/>
+
+      <div>Hello World</div>
+    </div>
   );
 }
 
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
 
+import {ReactComponent as CatalystLogo} from '../../assets/catalyst-logo.svg';
+
 import './header.styles.scss';
 
 const Header = () => (
-	
-)
+	<div className="header">
+		<div className="logo-container">
+			<CatalystLogo className="logo"/>
+		</div>
+
+		<div className="button-container">
+			<a>View Blog</a>
+		</div>
+
+	</div>
+);
+
+export default Header;
