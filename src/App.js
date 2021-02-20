dc0d9cd929b738b09fe4971e0a89dbef549682d1
Gweezdo <z.trivium@gmail.com>
2021-02-20 15:18:40 +1100

    This is a branching test commint

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Header from './components/header/header.component';
 import OrgSection from './sections/organisation/organisation.component';
 // import RepoSection from './sections/repositories/repositories.component';
+// This is a test on branching
 
 import './App.css';
 
